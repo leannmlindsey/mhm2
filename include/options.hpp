b132c14ba8fcce52687043c1300bae46d89ecc36
Rob Egan <RSEgan@lbl.gov>
2021-06-23 13:23:15 -0700

    changed shuffle reads to be default

--- include/options.hpp
+++ include/options.hpp
@@ -112,7 +112,7 @@ class Options {
   string output_dir;
   string setup_time;
   bool restart = false;
-  bool shuffle_reads = false;
+  bool shuffle_reads = true;
   bool dump_kmers = false;
 
   Options();
