50dc0966a04502254f8b6f45add82d76be9d397a
Rob Egan <rsegan@lbl.gov>
2021-09-15 13:50:40 -0700

    build with cray PrgEnv on perlmutter

---
+++ contrib/environments/perlmutter/cray.sh
@@ -0,0 +1,14 @@
+module load PrgEnv-cray
+module load cmake
+
+export PATH=/global/common/software/m2878/shasta2105/upcxx/TESTING-PrgEnv-cray/bin:$PATH
+
+module list
+which cc
+which CC
+which g++
+which gcc
+which nvcc
+which upcxx
+
+export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which cc) -DCMAKE_CXX_COMPILER=$(which CC) -DCMAKE_CUDA_COMPILER=$(which nvcc)"
