f9fa37d98a69cb065b7b3fcf6012aa9060393250
Rob Egan <rsegan@lbl.gov>
2021-09-17 23:59:45 -0700

    added env vars from Paul for conduit

--- contrib/environments/perlmutter/cray.sh
+++ contrib/environments/perlmutter/cray.sh
@@ -3,6 +3,9 @@ module load cmake
 
 export PATH=/global/common/software/m2878/shasta2105/upcxx/TESTING-PrgEnv-cray/bin:$PATH
 
+export UCX_TLS=dc
+export UCX_DC_MLX5_NUM_DCI=16
+
 module list
 which cc
 which CC
