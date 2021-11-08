94f794ce9be1826f8e7d720997d4bd8ccdd9b52b
Rob Egan <RSEgan@lbl.gov>
2021-11-08 16:10:37 -0500

    updated build and run environment on summit

--- contrib/environments/summit/gnu-bleedingedge.sh
+++ contrib/environments/summit/gnu-bleedingedge.sh
@@ -9,9 +9,9 @@ module load cuda
 module load upcxx
 module load python
 
-export GASNET_ODP_VERBOSE=0 # disable warnings
 export GASNET_RBUF_COUNT=32676 # maximum allowed currently support 778 nodes "fully provisioned for simultaneous all-to-all"
 export GASNET_AM_CREDITS_SLACK=0 # to disable warnings
 export GASNET_AM_CREDITS_PP=1 # for when 1 hca per rank
+
 export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which mpicc) -DCMAKE_CXX_COMPILER=$(which mpicxx) -DCMAKE_CUDA_COMPILER=$(which nvcc)"
 export MHM2_BUILD_THREADS=8
