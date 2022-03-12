784843ec2cbfc90a7fa61bd13f9c0c602b5f65b1
Rob Egan <RSEgan@lbl.gov>
2022-03-11 19:46:33 -0500

    new build environment

--- contrib/environments/crusher/gnu.sh
+++ contrib/environments/crusher/gnu.sh
@@ -10,7 +10,8 @@ module rm xl
 module load gcc
 #module load cuda
 #module load upcxx
-export PATH=/gpfs/alpine/csc296/world-shared/crusher/upcxx/gnu/11.2.0/nightly/bin:$PATH
+module use /gpfs/alpine/csc296/world-shared/crusher/modulefiles
+module load upcxx
 module list
 which upcxx
 
@@ -19,7 +20,7 @@ module load cray-python
 
 #export GASNET_ODP_VERBOSE=0 # disable warnings
 #export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which mpicc) -DCMAKE_CXX_COMPILER=$(which mpicxx) -DCMAKE_CUDA_COMPILER=$(which nvcc)"
-export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DENABLE_CUDA=Off"
+export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which cc) -DCMAKE_CXX_COMPILER=$(which CC) -DENABLE_CUDA=Off"
 export MHM2_BUILD_THREADS=8
 
 # salloc/sbatch with: -A BIF115_crusher --ntasks-per-node=64 
